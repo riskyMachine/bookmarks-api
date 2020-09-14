017fec5e3e64046d81f6d44dc68ba796654e7d25
riskyMachine <60124720+riskyMachine@users.noreply.github.com>
2020-09-14 21:33:26 +0530

    adding tags in getAllBookmarks

--- controllers/bookmark.controller.js
+++ controllers/bookmark.controller.js
@@ -1,5 +1,6 @@
 const Bookmark = require("../models/bookmark.model");
 const User = require("../models/user.model");
+const { getTags } = require('./tag.controller');
 
 async function createBookmark({ userId, link, title, publisher }) {
 	let bookmark = new Bookmark({ userId, link, title, publisher });
@@ -15,6 +16,10 @@ async function deleteBookmark({ userId, link }) {
 async function getAllBookmarks({ email }) {
 	let user = await User.findOne({ email });
 	let bookmarks = await Bookmark.find({ userId: user._id });
+	let tags = await getTags({ userId: bookmarks[0].userId });
+	bookmarks = JSON.parse(JSON.stringify(bookmarks));
+	bookmarks[0].tags = tags.tags.map(tag => tag.title);
+	console.log(bookmarks);
 	return bookmarks;
 }
 
